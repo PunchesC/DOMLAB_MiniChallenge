c6408cd4932fe5f11e01e930d2d6621ca0444ece
Curtis <c.b.punches@gmail.com>
2021-04-19 08:55:51 -0400

    first commit

---
+++ script1.js
@@ -0,0 +1,169 @@
+
+
+
+
+
+// let addition = document.querySelectorAll("input").onclick = function () {
+//   const totalSales =  document.getElementById("total");
+//   totalNumber = totalSales.value;
+//   for (let input of addition){
+//   itemInput.value += totalSales;
+//   console.log("a:" + totalSales);
+// }
+
+
+
+
+
+// return totalSales;
+
+
+
+
+// }
+
+
+// console.log(addition());
+
+let total=0;
+let inputValue = document.querySelectorAll("input")
+console.log(inputValue);
+// for (let input of inputValue){
+  // console.log(input);
+  cokeValue = inputValue[0].value;
+  console.log(cokeValue);
+  drPepperValue = inputValue[1].value;
+  console.log(drPepperValue);
+  spriteValue = inputValue[2].value;
+  console.log(spriteValue);
+  dietCokeValue = inputValue[3].value;
+  console.log(dietCokeValue);
+document.getElementById("coke").onclick = function addCook(){let el =document.getElementById("total.value");
+el += cokeValue;
+     
+      
+      
+    
+      
+    }
+  console.log(total);
+
+
+// =function addCokeValue(){ Number(cokeValue) += total;}
+// if(document.getElementById("coke").addEventListener("click",  function addCokeValue(){
+  //   console.log(input.value[0]);
+  // }));
+
+
+// const total= 0;
+// let input1 = document.getElementById("coke");
+// coke.addEventListener("click", (e)=> {
+  
+  
+//   total =+Number(input1.value);
+ 
+  
+
+
+// console.log(num0);
+
+// });
+
+
+// let input2 = document.getElementById("drPepper");
+// drPepper.addEventListener("click", (e)=> {
+  
+  
+//   total = Number(input2.value);
+ 
+  
+
+
+// console.log(num1);
+
+// });
+
+
+// let input3 = document.getElementById("sprite");
+// sprite.addEventListener("click", (e)=> {
+  
+  
+//   let num2 = Number(input3.value);
+ 
+  
+
+
+// console.log(num2);
+
+// });
+
+// let input4 = document.getElementById("dietCoke");
+// dietCoke.addEventListener("click", (e)=> {
+  
+  
+//   let num3 = Number(input4.value);
+ 
+  
+
+
+
+// console.log(num3);
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const totalSales = document.getElementById("total");
+// console.log(totalSales);
+// let totalNumber = totalSales.value;
+// console.log(totalSales.value);
+
+// document.getElementById("coke").onclick = function addCook(){
+//   let cokeValue = document.getElementById("coke");
+//   let cokeAdd = Number(cokeValue.value);
+//   // let cokeAdd = cokeValue.value; BadBAD
+
+//   totalSales.value += cokeAdd;
+// }
+
+
+// document.getElementById("drPepper").onclick = function addCook(){
+//   let drPepperValue = document.getElementById("drPepper");
+//   let drPepperAdd = Number(drPepperValue.value);
+
+//   totalSales.value += drPepperAdd ;
+// }
+
+// document.getElementById("sprite").onclick = function addCook(){
+//   let cokeValue = document.getElementById("sprite");
+//   let cokeAdd = Number(cokeValue.value);
+
+//   totalSales.value+= cokeAdd;
+// }
+
+// document.getElementById("dietCoke").onclick = function addCook(){
+//   let cokeValue = document.getElementById("dietCoke");
+//   let cokeAdd = Number(cokeValue.value);
+
+//   totalSales.value += cokeAdd;
+// }
+
---
+++ script3.js
@@ -0,0 +1,50 @@
+document.getElementById("on").onclick =function turnOnBright(){ let el=document.getElementById("lightbulb");
+el.classList.add("bright");}
+
+document.getElementById("off").onclick =function turnOffBright(){ let el=document.getElementById("lightbulb");
+el.classList.remove("bright");}
+
+document.getElementById("toggle").onclick =function turnToggleBright(){ let el=document.getElementById("lightbulb");
+el.classList.toggle("bright");}
+
+
+// button.disable=true
+
+let removeLight =document.getElementById("end").onclick =function removeLightbulb(){ let el=document.getElementById("lightbulb");
+el.remove();}
+
+
+// let removeLight =document.getElementById("end").onclick =function removeLightbulb(){ let el=document.getElementById("lightbulb");
+// el.remove();}
+
+// let uglyBorder= document.getElementById("end").onclick =function removeLightbulb(){ let el=document.getElementById("lightbulb");
+// el.classList.toggle("border");}
+
+document.getElementById("end").addEventListener("click", myFunction);
+
+function myFunction() {
+let removeLight =  document.getElementById("end").onclick =function removeLightbulb(){ let el=document.getElementById("lightbulb");}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// function endButton () {
+//     const a = removeLight;
+//     function ugly (){
+//       const b = uglyBorder;
+//     }
+//     ugly();
+
+// }
+// endButton();
+
+
